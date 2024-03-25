ef96eac1b1f4f6dacd4e13977ce236186cfa3e7c
Alex Korolkov <alexkorolkov88@gmail.com>
2024-03-25 17:13:00 +0300

    add Lesson2:Task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int [n];
+int i = 0;
+
+while (i < arr.Lenght) // Lenght - хранит количество элементов, под которое выделена память
+{
+    arr [i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(" ");
+    Console.Write($"{arr[i]} ");   // тоже, что и 2 строки выше, но одной строкой
+    i = i + 1;
+}
